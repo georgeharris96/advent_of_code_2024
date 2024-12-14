01d6c1efc65c81b68cd1314780b8feab252c5219
George Harris <george.s.harris@gmail.com>
2024-12-14 22:51:40 +0000

    day 3 part 2 WIP

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -1,29 +1,36 @@
 import re
+from itertools import chain
+
 def import_input(run=True):
     if run:
         with open('day-3/day-3-input.txt') as file:
-            lines = [line for line in file.readlines()]
+            lines = [line.strip("\n") for line in file.readlines()]
     else:
-        lines = ['xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))']
-    return lines
+        lines = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"]
+    return "".join(lines)
 
-def find_muls(lines):
-    muls = []
-    for line in lines:
-        muls.append(re.findall(r'mul\((-?\d+),(-?\d+)\)',line))
-    return muls
+def filter_text(text):
+    filtered_text = []
+    for segment in text.split("do()"):
+        filtered_text.append(segment.split("don't()")[0])
+    return filtered_text[0]
 
-def calc_muls(list_of_muls):
-    sum_of_muls = 0
-    for muls in list_of_muls:
-        for mul in muls:
-            sum_of_muls += int(mul[0])*int(mul[1])
-    print(f"sum_of_muls: {sum_of_muls}")
-    return sum_of_muls
+def find_mul_statements(text):
+    mul_statements = re.findall(r'mul\((-?\d+),(-?\d+)\)', text)
+    return mul_statements
+
+def calculate_answer(mul_statements):
+    answer = 0
+    for statement in mul_statements:
+        answer += int(statement[0]) * int(statement[1])
+    
+    print(f"Answer: {answer}")
+    return answer
 
 def main():
-    lines = import_input()
-    list_of_muls = find_muls(lines)
-    sum_of_muls = calc_muls(list_of_muls)
+    text = import_input()
+    filtered_text = filter_text(text)
+    mul_statements = find_mul_statements(filtered_text)
+    answer = calculate_answer(mul_statements)
 
 main()
